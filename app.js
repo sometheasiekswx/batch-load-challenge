fb1682b44d616705769303db26e8b70cec7d7b07
Somethea Siek <sometheasiekswx@gmail.com>
2023-05-17 23:07:55 +1000

    Import item stock

--- app.js
+++ app.js
@@ -6,92 +6,114 @@ const csv = require('csv-parser');
 require("dotenv").config();
 
 const client = new MongoClient(process.env.MONGODB_CONNECTION_STRING, {
-	serverApi: {
-		version: ServerApiVersion.v1, strict: true, deprecationErrors: true,
-	}
+    serverApi: {
+        version: ServerApiVersion.v1, strict: true, deprecationErrors: true,
+    }
 });
 
-const csvUrl = 'https://drive.google.com/uc?id=1MzkoUhjhd4wNgrQ84jZ-FF4GZCk6FBTd';
+const orderCSVUrl = 'https://drive.google.com/uc?id=1MzkoUhjhd4wNgrQ84jZ-FF4GZCk6FBTd';
+const stockCSVUrl = 'https://drive.google.com/uc?id=16KlCwz-q51VZnQ7xyOTvZwEpCNrKuPEr';
+
 const csvFilePath = 'data.csv';
 
 async function downloadCSVFromUrl(url, filePath) {
-	try {
-		const response = await axios.get(url, {responseType: 'stream'});
-		const stream = response.data.pipe(fs.createWriteStream(filePath));
+    try {
+        const response = await axios.get(url, {responseType: 'stream'});
+        const stream = response.data.pipe(fs.createWriteStream(filePath));
 
-		return new Promise((resolve, reject) => {
-			stream.on('finish', resolve);
-			stream.on('error', reject);
-		});
-	} catch (error) {
-		throw new Error(`Error downloading CSV: ${error.message}`);
-	}
+        return new Promise((resolve, reject) => {
+            stream.on('finish', resolve);
+            stream.on('error', reject);
+        });
+    } catch (error) {
+        throw new Error(`Error downloading CSV: ${error.message}`);
+    }
 }
 
 async function readCSVFromFile(filePath) {
-	try {
-		const results = [];
-		const stream = fs.createReadStream(filePath);
+    try {
+        const results = [];
+        const stream = fs.createReadStream(filePath);
 
-		return new Promise((resolve, reject) => {
-			stream
-				.pipe(csv())
-				.on('data', (row) => {
-					results.push(row);
-				})
-				.on('end', () => {
-					resolve(results);
-				})
-				.on('error', (error) => {
-					reject(new Error(`Error parsing CSV: ${error.message}`));
-				});
-		});
-	} catch (error) {
-		throw new Error(`Error reading CSV: ${error.message}`);
-	}
+        return new Promise((resolve, reject) => {
+            stream
+                .pipe(csv())
+                .on('data', (row) => {
+                    results.push(row);
+                })
+                .on('end', () => {
+                    resolve(results);
+                })
+                .on('error', (error) => {
+                    reject(new Error(`Error parsing CSV: ${error.message}`));
+                });
+        });
+    } catch (error) {
+        throw new Error(`Error reading CSV: ${error.message}`);
+    }
 }
 
 async function processCSVFromUrl(url) {
-	try {
-		await downloadCSVFromUrl(url, csvFilePath);
-		return await readCSVFromFile(csvFilePath);
-	} catch (error) {
-		console.error(error.message);
-		return null;
-	}
+    try {
+        await downloadCSVFromUrl(url, csvFilePath);
+        return await readCSVFromFile(csvFilePath);
+    } catch (error) {
+        console.error(error.message);
+        return null;
+    }
 }
 
 
 async function run() {
-	try {
-		const parsedData = await processCSVFromUrl(csvUrl);
-		if (parsedData) {
-			const database = client.db("batch-load-challenge-db");
-			const orders = database.collection("orders");
-			const customers = database.collection("customers");
+    try {
+        const database = client.db("batch-load-challenge-db");
+        const stock = database.collection("stock");
 
-			for (const record of parsedData) {
-				const customer = await customers.findOne({customerId: record.customerId});
-				if (!customer) {
-					continue;
-				}
+        const parsedStock = await processCSVFromUrl(stockCSVUrl);
+        if (parsedStock) {
+            const result = await stock.insertMany(parsedStock);
+            console.log(`${JSON.stringify(result)} ${JSON.stringify(stock)} inserted`)
+        }
 
-				const order = await orders.findOne({customerId: record.customerId, orderId: record.orderId});
-				if (order) {
-					console.log(`${order} already exists`)
-					continue;
-				}
+        const parsedData = await processCSVFromUrl(orderCSVUrl);
+        if (parsedData) {
+            const orders = database.collection("orders");
+            const customers = database.collection("customers");
 
-				const result = await orders.insertOne(record);
-				console.log(`${result} inserted`)
-			}
+            for (const record of parsedData) {
+                const customer = await customers.findOne({customerId: record.customerId});
+                if (!customer) {
+                    console.log(`customer does not exist`);
+                    continue;
+                }
 
-		} else {
-			console.log('Failed to parse CSV data');
-		}
-	} finally {
-		await client.close();
-	}
+                const order = await orders.findOne({customerId: record.customerId, orderId: record.orderId});
+                if (order) {
+                    console.log(`order already exists`);
+                    continue;
+                }
+
+                const stockLeft = await stock.findOne({item: record.item});
+                if (stockLeft.quantity <= 0) {
+                    console.log(`item out of stock`);
+                    continue;
+                }
+
+                if (stockLeft.quantity - record.quantity < 0) {
+                    console.log(`You ordered ${record.quantity}, but item only has ${stockLeft.quantity} left on stock`);
+                    continue;
+                }
+
+                const result = await orders.insertOne(record);
+                console.log(`${JSON.stringify(result)} ${JSON.stringify(record)} inserted`);
+            }
+
+        } else {
+            console.log('Failed to parse CSV data');
+        }
+    } finally {
+        await client.close();
+    }
 }
 
 run().catch(console.dir);
